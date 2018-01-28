733496669cbb8970e997557cacefd15489805ed4
Joe Harding <joe.harding@us.ibm.com>
2018-01-28 15:15:44 -0600

    basic shape moving around screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,17 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[])
+{
   SDL_Window* window = NULL;
   SDL_Surface* screenSurface = NULL;
+  SDL_Surface* redBlock = NULL;
+
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
     return 1;
   }
-  window = SDL_CreateWindow(
-			    "hello_sdl2",
+  window = SDL_CreateWindow("ragemash",
 			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
 			    SCREEN_WIDTH, SCREEN_HEIGHT,
 			    SDL_WINDOW_SHOWN
@@ -24,7 +26,57 @@ int main(int argc, char* args[]) {
   screenSurface = SDL_GetWindowSurface(window);
   SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
   SDL_UpdateWindowSurface(window);
-  SDL_Delay(3000);
+
+  redBlock = SDL_LoadBMP("assets/red.bmp");
+  if(redBlock == NULL)
+  {
+    fprintf(stderr, "ERROR: couldn't load red.bmp");
+  }
+
+	SDL_Rect location = {0,0,600,400};
+	
+	//Main loop flag
+	bool quit = false;
+
+	//Event handler
+	SDL_Event e;
+
+	//While application is running
+	while( !quit )
+	{
+		//Handle events on queue
+		while( SDL_PollEvent( &e ) != 0 )
+		{
+			//User requests quit
+			switch( e.type )
+			{
+				case SDL_QUIT:
+					quit = true;
+					printf("got the quit event\n");
+					break;
+				case SDL_KEYDOWN:
+					if(e.key.keysym.sym == SDLK_ESCAPE)
+					{
+						quit = true;
+						printf("got the quit event\n");
+					}
+					break;
+			}
+		}
+
+		location.x++;
+		if(location.x > SCREEN_WIDTH)
+		{ location.x = 0;
+		}
+  	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+		SDL_BlitSurface( redBlock, NULL, screenSurface, &location );
+		SDL_UpdateWindowSurface(window);
+
+		//Update the surface
+		SDL_UpdateWindowSurface( window );
+	}
+ 
+  //SDL_Delay(3000);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
